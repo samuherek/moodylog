8448bbc9f4994d09b02bd0182b0ec2e53ad869f0
Sam Uherek <samuherekbiz@gmail.com>
2018-08-02 15:02:34 +0200

    added more stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { isAuthenticated } from './firebase';
 
-const {app} = window.require('electron').remote;
+// const { ipcRenderer } = window.require('electron');
+
+// const { app } = window.require('electron').remote;
 
 class App extends Component {
+  componentDidMount() {
+    // let n = new Notification('You did it!', {
+    //   body: 'Nice work.'
+    // });
+    // console.log(n);
+    // n.onclick = () => {
+    //   ipcRenderer.send('show-window');
+    // };
+  }
+
   render() {
+    if (!isAuthenticated()) {
+      return <div>SIgn up first</div>;
+    }
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React + Electron = <span role="img" aria-label="love">😍</span></h2>
+          <h2>
+            Reacton + Electron ={' '}
+            <span role="img" aria-label="love">
+              😍
+            </span>
+          </h2>
         </div>
         <p className="App-intro">
           <b> Release 0.2.7 </b>
-          Version: {app.getVersion()}
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { auth, uidKey, dataKey, timestampKey, firestore, isAuthenticated } from './firebase';
+
+const startListeningToAuthChanges = () => {
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log('user', user);
+    }
+  });
+};
+
+startListeningToAuthChanges();
+
 ReactDOM.render(<App />, document.getElementById('root'));
